65678156c62d11ed82cca35ecd90693aad8a4acc
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-01-24 22:04:31 +0600

    Simple todo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,43 @@
 <template>
-  <h1>Hello Vue js</h1>
+    <div class="container">
+        <h1>Todos</h1>
+        <ul>
+            <li v-for="todo in todos" :key="todo">
+               {{todo}}     
+            </li>
+        </ul>
+        <input v-model="newTodo" placeholder="todo title">
+        <p class="error">{{error}}</p>
+        <button @click="addTodo">Add Todo</button>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  props: {
-    msg: String
+  data() {
+    return {
+        todos: ['Initial todo'],
+        newTodo: '',
+        error: '',
+    }
+  },
+  methods: {
+      addTodo() {
+          if (this.newTodo.trim().length === 0) {
+              return this.error = "Title is required";
+          }
+
+          this.todos.push(this.newTodo);
+          this.error = '';
+          this.newTodo = '';
+      }
   }
 }
 </script>
+
+<style scoped>
+    .error {
+        color: red;
+    }
+</style>
