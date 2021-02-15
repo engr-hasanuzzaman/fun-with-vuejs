fc2b26bec0fd4963b666a4e8bdb66b6c95961fba
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-02-16 01:20:05 +0600

    Experimenting lifecycle hook, watch & computer property

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>The number of clicked is: {{tenTimeCounter}}</p>
+        <button @click="$emit('clicked')"></button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Clock",
+    props: {
+        clickCounter: {
+            type: Number,
+            required: true,
+        }
+    }
+    ,
+    watch: {
+        clickCounter() {
+
+        }
+    },
+    computed: {
+        tenTimeCounter: {
+            get: () => {
+                return this.clickCounter + 10;
+            },
+            set: (clickCounter) => {
+                console.log("new counter value is ", clickCounter);
+            }
+            
+        }
+    }
+}
+</script>
