0ab5dbacc0fe1b0929d7748f703ad17e1d651ab1
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-02-20 02:11:30 +0600

    Install vuex for state management

---
+++ src/store/modules/todos.js
@@ -0,0 +1,31 @@
+const state = {
+    todos: [
+        {
+            title: "Vuex1 title",
+            body: "Vuex1 title",
+            userId: 1,
+            id: 1,
+        },
+        {
+            title: "Vuex1 title",
+            body: "Vuex1 title",
+            userId: 1,
+            id: 2,
+        }
+    ]
+};
+
+const getters = {
+    todos: (state) => state.todos,
+};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+    state,
+    actions,
+    mutations,
+    getters,
+};
