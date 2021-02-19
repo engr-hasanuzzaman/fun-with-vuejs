5749dcf97ae8ce08c989dd98979b2e9adca045d0
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-02-19 23:10:43 +0600

    Call api to create new post

--- src/components/post/Api.js
+++ src/components/post/Api.js
@@ -6,3 +6,19 @@ export const fetchPosts = async () => {
         return [];
     }
 }
+
+export const createPost = async (post) => {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+          },
+        body: JSON.stringify(post)
+    });
+
+    if (resp.ok) {
+        return await resp.json()
+    } else {
+        return {};
+    }
+}
--- src/components/post/Form.vue
+++ src/components/post/Form.vue
@@ -2,18 +2,38 @@
     <div class="m-20">
         <div class="flex flex-col mb-4">
             <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="title">Title</label>
-            <input class="border py-2 px-3 text-grey-darkest" type="text" name="title" id="title">
+            <input class="border py-2 px-3 text-grey-darkest" type="text" name="title" id="title" v-model="post.title" required>
         </div>
         <div class="flex flex-col mb-4">
             <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="body">Body</label>
-            <textarea class="border py-2 px-3 text-grey-darkest" type="text" name="body" id="body" rows="4" cols="12" />
+            <textarea class="border py-2 px-3 text-grey-darkest" type="text" name="body" id="body" rows="4" cols="12" v-model="post.body" required/>
         </div>
-        <button class="block bg-blue-200 hover:bg-teal-dark uppercase text-lg mx-auto p-4 rounded" type="submit">Create Post</button>
+        <button @click="postCreate" class="block bg-blue-200 hover:bg-teal-dark uppercase text-lg mx-auto p-4 rounded" type="submit">Create Post</button>
     </div>
 </template>
 
 <script>
+import { createPost } from './Api';
+
 export default {
-    name: 'NewPost'
+    name: 'NewPost',
+    data() {
+        return {
+            post: {
+                title: '',
+                body: '',
+                userId: 1,
+            }
+        }
+    },
+    methods: {
+        async postCreate() {
+            const post = await createPost(this.post)
+            
+            if (Object.keys(post).length > 0) {
+                alert("Post created successfully");
+            }
+        }
+    }
 }
 </script>
