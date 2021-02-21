bb776ac7c721ba238d13acb986dabd1b011357d0
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-02-21 19:22:49 +0600

    formatting post format and index

--- src/components/post/Form.vue
+++ src/components/post/Form.vue
@@ -1,40 +1,69 @@
 <template>
-    <div class="m-20">
-        <form @submit.prevent="handleSubmit">
-        <div class="flex flex-col mb-4">
-            <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="title">Title</label>
-            <input class="border py-2 px-3 text-grey-darkest" type="text" name="title" id="title" v-model="post.title" required>
-        </div>
-        <div class="flex flex-col mb-4">
-            <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="body">Body</label>
-            <textarea class="border py-2 px-3 text-grey-darkest" type="text" name="body" id="body" rows="4" cols="12" v-model="post.body" required/>
-        </div>
-        <button class="block bg-blue-200 hover:bg-teal-dark uppercase text-lg mx-auto p-4 rounded" type="submit">Create Post</button>
+  <div class="m-20">
+    <form @submit.prevent="handleSubmit">
+      <div class="flex flex-col mb-4">
+        <label
+          class="mb-2 uppercase font-bold text-lg text-grey-darkest"
+          for="title"
+          >Title</label
+        >
+        <input
+          class="border py-2 px-3 text-grey-darkest"
+          type="text"
+          name="title"
+          id="title"
+          v-model="post.title"
+          required
+        />
+      </div>
+      <div class="flex flex-col mb-4">
+        <label
+          class="mb-2 uppercase font-bold text-lg text-grey-darkest"
+          for="body"
+          >Body</label
+        >
+        <textarea
+          class="border py-2 px-3 text-grey-darkest"
+          type="text"
+          name="body"
+          id="body"
+          rows="4"
+          cols="12"
+          v-model="post.body"
+          required
+        />
+      </div>
+      <button
+        class="block bg-blue-200 hover:bg-teal-dark uppercase text-lg mx-auto p-4 rounded"
+        type="submit"
+      >
+        Create Post
+      </button>
     </form>
-    </div>
+  </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
 
 export default {
-    name: 'NewPost',
-    data() {
-        return {
-            post: {
-                title: '',
-                body: '',
-                userId: 1,
-            }
-        }
+  name: "NewPost",
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+        userId: 1,
+      },
+    };
+  },
+  methods: {
+    ...mapActions(["createTodo"]),
+    handleSubmit(e) {
+      e.preventDefault();
+      this.createTodo(this.post);
+      this.$router.push("/posts");
     },
-    methods: {
-        ...mapActions(['createTodo']),
-        handleSubmit (e) {
-            e.preventDefault();
-            this.createTodo(this.post);
-            this.$router.push('/posts')
-        }
-    }
-}
+  },
+};
 </script>
--- src/components/post/Index.vue
+++ src/components/post/Index.vue
@@ -1,42 +1,47 @@
 <template>
-    <div class="container">
-        <Header />
-        <h1 class="text-xl text-center my-2 font-bold">List of fetch posts</h1>
-        <ul>
-            <li v-for="post in allTodos" :key="post.id" class="border-2 border-purple-100 m-2 display flex"> 
-                <p class="p-4">{{post.title}}</p>
-                <button @click="handleDeletePost(post.id)" class="text-red-500">Delete</button>
-            </li>
-        </ul>
-    </div>
+  <div class="container">
+    <Header />
+    <h1 class="text-xl text-center my-2 font-bold">List of fetch posts</h1>
+    <ul>
+      <li
+        v-for="post in allTodos"
+        :key="post.id"
+        class="border-2 border-purple-100 m-2 display flex"
+      >
+        <p class="p-4">{{ post.title }}</p>
+        <button @click="handleDeletePost(post.id)" class="text-red-500">
+          Delete
+        </button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-
-import Header from '../Header';
-import {mapGetters, mapActions} from 'vuex';
+import Header from "../Header";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-    name: "PostsPage",
-    data() {
-        return { }
+  name: "PostsPage",
+  data() {
+    return {};
+  },
+  methods: {
+    ...mapActions(["fetchTodos", "deletePost"]),
+    handleDeletePost(id) {
+      this.deletePost(id);
     },
-    methods: {
-        ...mapActions(['fetchTodos', 'deletePost']),
-        handleDeletePost(id) {
-            this.deletePost(id);
-        }
-    },
-    created: async function () {
-        if (this.$store.state.Todos.todos.length == 0){
-            this.fetchTodos ();
-        }
-    },
-    components: {
-        Header
-    },
-    computed: {
-        ...mapGetters(['allTodos']),
-    },
-}
+  },
+  created: async function () {
+    if (this.$store.state.Todos.todos.length == 0) {
+      this.fetchTodos();
+    }
+  },
+  components: {
+    Header,
+  },
+  computed: {
+    ...mapGetters(["allTodos"]),
+  },
+};
 </script>
