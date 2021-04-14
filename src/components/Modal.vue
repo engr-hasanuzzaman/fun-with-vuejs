dd21879fa8387da85ad01b5e000321ffb98e9585
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-14 22:50:22 +0600

    Start creating modal view using slot

---
+++ src/components/Modal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="header">
+      <slot name="headerContent"></slot>
+    </div>
+    <div class="body">
+      <slot name="headerContent"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
