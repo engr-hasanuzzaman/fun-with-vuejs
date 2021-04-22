5794e0c7c7d78d15866f9f51b5a649ed1f3ca90c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-22 19:12:09 +0600

    Update the home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,59 +1,77 @@
 <template>
-    <div>
-        <Header />
-        <div class="w-md mx-auto flex space-x-10 mt-10 px-10">
-            <div class="flex-1">
-                <TodoForm @newTodoAdded="addTodo"></TodoForm>
-            </div>
-            <TodoList title="Pending" :todos="inCompletedTodo" @clicked="toggleStatus"></TodoList>
-            <TodoList title="Completed" :todos="completedTodo" @clicked="toggleStatus"></TodoList>
+  <div>
+    <Header />
+    <div class="w-md mx-auto flex space-x-10 mt-10 px-10">
+      <Modal v-if="modal">
+        <h4 slot="header">New Todo</h4>
+        <TodoForm @newTodoAdded="addTodo" slot="body"></TodoForm>
+        <div slot="footer">
+          <button @click="toggleModal">Close</button>
         </div>
+      </Modal>
+      <div class="flex-1">
+        <button @click="toggleModal">Add new</button>
+      </div>
+      <TodoList
+        title="Pending"
+        :todos="inCompletedTodo"
+        @clicked="toggleStatus"
+      ></TodoList>
+      <TodoList
+        title="Completed"
+        :todos="completedTodo"
+        @clicked="toggleStatus"
+      ></TodoList>
     </div>
-    
+  </div>
 </template>
 
 <script>
-import TodoList from './TodoList';
-import TodoForm from './NewTodo';
-import Header from './Header';
+import TodoList from "./TodoList";
+import TodoForm from "./NewTodo";
+import Header from "./Header";
+import Modal from "./Modal";
 
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
-        todos: [
-            {title: "Sleep", status: false }
-            ],
-        title: 'custom title',
-        mainTitle: "Remaining todos",
-    }
+      todos: [{ title: "Sleep", status: false }],
+      title: "custom title",
+      mainTitle: "Remaining todos",
+      modal: false,
+    };
   },
   methods: {
-     toggleStatus (todo) {
-         todo.status = !todo.status;
-     },
-     addTodo(todo) {
-         this.todos = [...this.todos, {...todo}];
-     }
+    toggleStatus(todo) {
+      todo.status = !todo.status;
+    },
+    addTodo(todo) {
+      this.todos = [...this.todos, { ...todo }];
+    },
+    toggleModal() {
+      this.modal = !this.modal;
+    },
   },
   computed: {
-      completedTodo () {
-          return this.todos.filter(t => t.status);
-      },
-      inCompletedTodo () {
-          return this.todos.filter(t => !t.status);
-      }
+    completedTodo() {
+      return this.todos.filter((t) => t.status);
+    },
+    inCompletedTodo() {
+      return this.todos.filter((t) => !t.status);
+    },
   },
   components: {
-      TodoList,
-      TodoForm,
-      Header,
-  }
-}
+    TodoList,
+    TodoForm,
+    Header,
+    Modal,
+  },
+};
 </script>
 
 <style scoped>
-    .error {
-        color: red;
-    }
+.error {
+  color: red;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 import Todos from '../components/Home';
 import PostsPage from '../components/post/Index';
 import NewPost from '../components/post/Form';
-
+// import Home from '../components/Ho'
 Vue.use(Router)
 
 const routes = [
@@ -11,6 +11,7 @@ const routes = [
     { path: '/posts', component: PostsPage, name: "PostPath" },
     { path: '/posts/new', component: NewPost, name: "PostNew" },
     { path: '/todos', component: Todos, name: "TodosPath"},
+    { path: '/home', component: Todos, name: "TodosPath"},
  ];
  
 export default new Router({
