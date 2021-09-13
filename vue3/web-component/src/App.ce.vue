48075ee8aaf31617e3c7e9aa0df3250adee7a463
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-09-14 01:01:01 +0600

    Basic todo status check with production build

--- vue3/web-component/src/App.ce.vue
+++ vue3/web-component/src/App.ce.vue
@@ -1,30 +1,48 @@
 <template>
-  <div>
+  <div class="container">
     <TodoElement
       v-for="todo in todos"
       :key="todo.id"
       :title="todo.title"
-      :status="status"
+      :status="todo.status"
+      @onComplete="handleComplete(todo.id)"
     />
   </div>
 </template>
 
 <script>
 import { reactive } from "vue";
-import TodoElement from "./components/TodoElement.ce.vue";
+import TodoElement from "./components/TodoElement.vue";
 
 export default {
   setup() {
-    const todos = reactive([
+    const initialData = [
       {
         title: "Sample todo",
         status: false,
         id: 1,
       },
-    ]);
+      {
+        title: "Sample todo complete",
+        status: true,
+        id: 2,
+      },
+    ];
+    const state = reactive({
+      todos: [...initialData]
+    });
 
+    // methods
+    function handleComplete(id) {
+      this.todos.forEach(todo => {
+        if(todo.id === id) {
+          todo.status = !todo.status;
+        }
+      });
+    }
     return {
-      todos,
+      ...state,
+      handleComplete
     };
   },
   components: {
@@ -34,9 +52,22 @@ export default {
 </script>
 
 <style>
+.container {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
 .todo-item {
   display: flex;
   border: 1px solid blue;
   padding: 10px;
+  align-items: center;
+  justify-content: start;
+  gap: 10px;
+}
+
+.todo-item--complete {
+  text-decoration: line-through;
 }
 </style>
---
+++ vue3/web-component/src/components/TodoElement.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="todo-item">
+    <p :class="status ? 'todo-item--complete' : ''">{{ title }}</p>
+    <input
+      type="checkbox"
+      name="status"
+      @click="$emit('onComplete')"
+      :checked="status"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+defineEmits(["onComplete"]);
+</script>
